bd4e8387cd76a4dc9d13ccc45fe7841aef75f1e4
Alexander G. Morano <amorano@gmail.com>
2025-06-17 13:32:29 -0400

    base commit

---
+++ __init__.py
@@ -0,0 +1,58 @@
+"""
+@title: cozy_combo_dynamic
+@author: amorano
+@category: Example
+@reference: https://github.com/cozy-comfyui/cozy_combo_dynamic
+@tags: dynamic, example, developer, script, mechanism, exemplar
+@description: Example of a Combo widget getting its values from being connected
+to another combo widget
+@node list:
+    ComboDynamicNodeCozy
+@version: 1.0.0
+"""
+
+# =============================================================================
+# === GLOBAL ===
+# =============================================================================
+
+from typing import Any
+
+
+NODE_CLASS_MAPPINGS = {}
+NODE_DISPLAY_NAME_MAPPINGS = {}
+WEB_DIRECTORY = "./web"
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
+
+# =============================================================================
+# === NODE ===
+# =============================================================================
+
+class ComboDynamicNodeCozy:
+    """
+    A class to represent a dynamic combo changer in ComfyUI.
+    """
+    RETURN_TYPES = ()
+    RETURN_NAMES = ()
+    CATEGORY = "_EXAMPLES"
+
+    @classmethod
+    def INPUT_TYPES(s) -> dict[str, tuple[Any]]:
+        return {
+            "required": {},
+            "optional": {
+                "combo": ([], {"default": ""}),
+                "text": ("STRING", {"default": "", "multiline": True})
+            }
+        }
+
+# =============================================================================
+# === REGISTRATION ===
+# =============================================================================
+
+NODE_CLASS_MAPPINGS = {
+    "ComboDynamicNodeCozy": ComboDynamicNodeCozy,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "ComboDynamicNodeCozy": "Combo Dynamic (cozy)",
+}
